7bd8931c04e225ec12a1c9de8fe530ba9f67b59c
Frances Tang <francest221@gmail.com>
2021-07-27 03:01:09 -0700

    Add index.html template

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/mood')
+def mood():
+    return 'Welcome to Reddit Mood Checker!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
