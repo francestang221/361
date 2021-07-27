6a1565cd32b623842e1be588c04a14bd3c4c4995
Frances Tang <francest221@gmail.com>
2021-07-27 05:45:15 -0700

    Added signup and mood checker forms

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import (Flask, render_template,
+                   url_for, request)
 
 app = Flask(__name__)
 
@@ -8,14 +9,16 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    print(request.form)
     return render_template('signup.html')
 
 
-@app.route('/mood')
+@app.route('/mood', methods=['GET', 'POST'])
 def mood():
-    return  render_template('mood.html')
+    print(request.form)
+    return render_template('mood.html')
 
 
 @app.route('/searches')
