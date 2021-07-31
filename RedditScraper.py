ab211ac6cbb172d43c59921dfdf33c7e663e495f
Frances Tang <francest221@gmail.com>
2021-07-31 00:39:03 -0700

    Modified test scraper

--- RedditScraper.py
+++ RedditScraper.py
@@ -1,11 +1,31 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-test_scrape = urlopen('https://www.reddit.com/r/OSUOnlineCS/search?q=cs%20225&restrict_sr=1')
+subreddit = 'OSUOnlineCS'
+keyword = 'CS 225'
+
+
+def parse_keyword(kw):
+    ans = ""
+    for i in kw:
+        if i == " ":
+            ans += "%20"
+        else:
+            ans += i
+    return ans
+
+
+query = parse_keyword(keyword)
+
+test_scrape = urlopen('https://www.reddit.com/r/' + subreddit + '/search?q=' + query + '&restrict_sr=1')
 
 # create BS object
 soup = BeautifulSoup(test_scrape.read(), 'html.parser')
 
-data = soup.findAll()
+data = soup.prettify()
 
 print(data)
+
+# "title"
+# "permalink"
+
