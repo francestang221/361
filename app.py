6f5a4f38ed6b0258c679d308cc07ea4321dc91df
Frances Tang <francest221@gmail.com>
2021-08-04 06:21:55 -0700

    update

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ HOST = '0.0.0.0'
 app = Flask(__name__)
 app.secret_key = 'secret'
 
+""" comment out for mvp deployment
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
@@ -28,30 +29,34 @@ def load_user(userid):
 
 @app.before_request
 def before_request():
-    """Connect to the database before each request """
+    #Connect to the database before each request
     g.db = models.DATABASE
     g.db.connect()
 
 
 @app.after_request
 def after_request(response):
-    """Close the database connection after each request"""
+    #Close the database connection after each request
     g.db.close()
     return response
 
+"""
 
 # Views
+
+
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     form = forms.RegisterForm()
     if form.validate_on_submit():
         flash("Yay, you registered!", "success")
+        """
         models.User.create_user(
             username=form.username.data,
             email=form.email.data,
             password=form.password.data
         )
-        return redirect(url_for('index2'))
+        """
     return render_template('register.html', form=form)
 
 
